669ae1239f5f2f5f9fb05d800288f224731d207d
Jacky Huang <jacky_huang56@hotmail.com>
2021-01-17 00:54:28 -0700

    timer stopped but time is off

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ def main():
     pygame.display.update()
 
     #pause 3 seconds before starting
-    pygame.time.delay(TIME_DELAY)
+    pygame.time.wait(TIME_DELAY)
+    print(pygame.time.get_ticks())
     clearScreen(screen)
     #draw rectangles
     for i in range(0, numTargets):
@@ -40,8 +41,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            else:
-                displayTimer(screen, font, completed)
+            elif not completed:
+                displayTimer(screen, font)
                 pos = pygame.mouse.get_pos()
                 if event.type == pygame.MOUSEBUTTONUP:
                     totalClicks += 1
@@ -54,16 +55,14 @@ def main():
                                 pygame.draw.rect(screen, GREEN, rect, 0)
                             pygame.display.update()
                             numTargets -= 1
-                if numTargets == 0:
-                    total_time = (pygame.time.get_ticks() - start - TIME_DELAY) / 1000
-                    pygame.time.delay(5000)
-                    completed = True
-                    accuracy = (accurateClicks / totalClicks) * 100
-                    stopTimer(screen, font, total_time)
-                    displayAccuracy(screen, font, accuracy)
-                
-
-
+                            if numTargets == 0:
+                                total_time = (pygame.time.get_ticks() - TIME_DELAY - start)
+                                print(total_time)
+                                completed = True
+                                accuracy = (accurateClicks / totalClicks) * 100
+                                stopTimer(screen, font, total_time)
+                                displayAccuracy(screen, font, accuracy)
+        
 #returns a random pygame.rect object
 def generateRandRect():
     width = random.randint(50,100)
@@ -86,19 +85,17 @@ def displayInstructions(screen, font, numTargets):
 
     return
 
-def displayTimer(screen, font, completed):
+def displayTimer(screen, font):
     pygame.draw.rect(screen, BLACK, (WINDOW_WIDTH-100,0,100,50))
-    text = font.render("%d"%(pygame.time.get_ticks() - 3000), True, WHITE, BLACK)
+    text = font.render("%d"%(pygame.time.get_ticks() - TIME_DELAY), True, WHITE, BLACK)
     text_rect = text.get_rect()
     text_rect.center = (WINDOW_WIDTH-text_rect.width//2, 30)
     screen.blit(text, text_rect)
-    if not completed:
-        pygame.display.update()
+    pygame.display.update()
     
     return
 
 def stopTimer(screen, font, time):
-    print(time)
     pygame.draw.rect(screen, BLACK, (WINDOW_WIDTH-100,0,100,50))
     text = font.render(str(time), True, WHITE, BLACK)
     text_rect = text.get_rect()
@@ -113,6 +110,7 @@ def displayAccuracy(screen, font, accuracy):
     textRect = text.get_rect()
     textRect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
     screen.blit(text, textRect)
+    pygame.display.update()
 
 def clearScreen(screen):
     screen.fill(BLACK)
